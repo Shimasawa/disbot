8d0be22bdcbee3915fe9cf704acf586a844785a2
shimasawa <honnomushi@rakuten.jp>
2022-03-11 11:48:41 +0900

    名前を利用した所持額確認っコマンド・ヘルプコマンドの実装

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ with open("databox/token.json","r") as f:
     token_json = json.load(f)
 
 TOKEN = token_json["TOKEN"]
+intents = discord.Intents.default()
 
-bot = commands.Bot(command_prefix="k!")
+bot = commands.Bot(command_prefix="k!",intents=intents)
 
 bot.load_extension("Cogs.sum_num")
 
